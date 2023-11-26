2076e905c2d476bec184574685388e904d09c01f
Gerald-herrerab <134649587+Gerald-herrerab@users.noreply.github.com>
2023-11-26 19:20:22 -0300

    aa

--- src/main/UI.java
+++ src/main/UI.java
@@ -10,44 +10,16 @@ import object.OBJ_Key;
 
 public class UI {
 
-		
-	}
-	
-	public void showMessage(String text) {
-		
-		message = text;
-		messageOn = true;
-		
-	}
-	
-	public void draw(Graphics2D g2) {
-		this.g2 = g2;
-		g2.setFont(arial_40);
-		g2.setColor(Color.white);
+    GamePanel gp;
+    Graphics2D g2;
+    Font arial_40,  arial_80B;
+    public boolean messageOn = false;
+    public String message = "";
+    int messageCounter = 0;
+    public boolean gameFinish = false;
 
-		if (gp.GameState == gp.PlayState) {
-			
-		}
-		if (gp.GameState == gp.PauseState) {
-			DrawPauseScreen();
-		}
-	}
-
-	public void DrawPauseScreen(){
-		String text = "Pausa";
-		
-		int x = getXforCenterText(text);
-		int y = gp.screenHeigth/2;
-
-		g2.drawString(text, x, y);
-	}
-	public int getXforCenterText(String text){
-				int length = (int)g2.getFontMetrics().getStringBounds(text, g2).getWidth();
-		
-		int x = gp.screenWidth/2 - length/2;
-		return x;
-	}
-}
+    double playTime;
+    DecimalFormat dFormat = new DecimalFormat("#0.00");
 
 
 
@@ -68,9 +40,6 @@ public class UI {
     }
 
     public void draw(Graphics2D g2) {
-
-
-
         this.g2 = g2;
         g2.setFont(arial_40);
         g2.setColor(Color.white);
