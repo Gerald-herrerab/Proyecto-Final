5a0f0d7f8da6ddfdb6f4ed5d2f81f1bc4de90f53
Gerald-herrerab <134649587+Gerald-herrerab@users.noreply.github.com>
2023-11-26 17:40:54 -0300

    update tile y mapa  12

--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -22,23 +22,60 @@ public class TileManager {
 	public TileManager(GamePanel gp) {
 		
 		this.gp = gp;
-		tile = new Tile[10];
+		tile = new Tile[50];
 		
 		mapTileNum = new int [gp.maxWorldCol][gp.maxWorldRow];
 		
 		
 		getTileImage();
-		LoadMap("/maps/world01.txt");
+		LoadMap("/maps/worldV2.txt");
 		
 	}
 	
 	public void getTileImage() {
 		setup(0, "grass", false);
-		setup(1, "wall", true);
+		setup(1, "wall", false);
 		setup(2, "water", true);
 		setup(3, "earth", false);
 		setup(4, "tree", true);
-		setup(5, "sand", false);	
+		setup(5, "sand", false);
+		setup(6, "grass", false);
+		setup(7, "grass", false);
+		setup(8, "grass", false);
+		setup(9, "grass", false);
+		
+		setup(10, "grass00", false);
+		setup(11, "grass01", false);
+		setup(12, "water00", true);
+		setup(13, "water01", true);
+		setup(14, "water02", true);
+		setup(15, "water03", true);
+		setup(16, "water04", true);
+		setup(17, "water05", true);
+		setup(18, "water06", true);
+		setup(19, "water07", true);
+		setup(20, "water08", true);
+		setup(21, "water09", true);
+		setup(22, "water10", true);
+		setup(23, "water11", true);
+		setup(24, "water12", true);
+		setup(25, "water13", true);
+		setup(26, "road00", false);
+		setup(27, "road00", false);
+		setup(28, "road00", false);
+		setup(29, "road00", false);
+		setup(30, "road00", false);
+		setup(31, "road00", false);
+		setup(32, "road00", false);
+		setup(33, "road00", false);
+		setup(34, "road00", false);
+		setup(35, "road00", false);
+		setup(36, "road00", false);
+		setup(37, "road00", false);
+		setup(38, "road00", false);
+		setup(39, "earth", false);
+		setup(40, "wall", true);
+		setup(41, "tree", true);
 	}
 	
 	public void setup(int index, String imageName, boolean collision ) {
